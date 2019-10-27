9f88ba1b8b0e8d58fb750188d58961eefe376f58
Alexander Papirnyk <43519694+Gennerys@users.noreply.github.com>
2019-10-27 21:03:39 +0300

    added DA layer

---
+++ JojoMarket/JojoMarket.DAL/Entities/Category.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JojoMarket.DAL.Entities
+{
+    class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [NotMapped]
+        public bool CanBeDeleted
+        {
+            get
+            {
+                if (this.Products == null ||
+                    this.Products.Where(p => p.IsActive == true).Count() == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ JojoMarket/JojoMarket.DAL/Entities/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+
+namespace JojoMarket.DAL.Entities
+{
+    class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Manufacturer { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string UnitMeasure { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue,
+            ErrorMessage = "Please enter a positive price")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
